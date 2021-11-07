ef4f1a74b9da2ec68d3d8b2f916bac56709aa37c
unknown <msancho.akx@gmail.com>
2021-11-07 14:04:21 +0100

    first

---
+++ bases-node/helpers/multiplicar.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+const crearArchivo = (base = 5) => {
+    return new Promise((resolve, reject) => {
+        try {
+
+            console.log('=====================');
+            console.log(`Tabla del: ${base}`);
+            console.log('=====================');
+
+            let salida = '';
+
+            for (let i = 0; i <= 10; i++) {
+                salida += `${base} x ${i} = ${base * i}\n`;
+            }
+
+            console.log(salida);
+            fs.writeFileSync(`tabla-${base}.txt`, salida);
+            resolve(`tabla-${base}.txt`);
+
+        } catch (err) {
+            throw err;
+        }
+
+    })
+
+}
+
+module.exports = {
+    crearArchivo
+}
+
