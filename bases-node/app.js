50650810a2c6b8a4363c47ceac4c50670dbaa315
unknown <msancho.akx@gmail.com>
2021-11-07 16:57:34 +0100

    first

--- bases-node/app.js
+++ bases-node/app.js
@@ -5,8 +5,15 @@ console.clear()
 //5 x 2 =10
 //5 x 3 =15
 
-const base = 17;
+console.log(process.argv);
+
+const [,,arg3 ='base=5'] = process.argv;
+const [, base=5] = arg3.split('=');
+console.log(base);
+
+//const base = 17;
 
 crearArchivo(base)
     .then(nombreArchivo => console.log(nombreArchivo, 'creado'))
     .catch(err => console.log(err));
+    
