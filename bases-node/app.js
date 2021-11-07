6812f7130c3bba49569a0dafc9ea4d4fbbdbc746
unknown <msancho.akx@gmail.com>
2021-11-07 13:46:06 +0100

    tablas

--- bases-node/app.js
+++ bases-node/app.js
@@ -1,9 +1,21 @@
+const fs = require('fs');
+
 //5 x 1 =5
 //5 x 2 =10
 //5 x 3 =15
 
-const base = 5;
+const base = 3;
+let salida = '';
 
 for (let i = 0; i <= 10; i++) {
-    console.log(`${base} x ~{i} = ${base * i}`);
+    salida += `${base} x ${i} = ${base * i}\n`;
 }
+
+console.log(salida);
+
+
+fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+    if (err) throw err;
+    console.log(`tabla-${base}.txt creada`);
+});
+
