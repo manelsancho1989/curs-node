662875621b4b224be8ef36ebc05298f1e4133f7d
Manel Sancho <manelsancho1989@gmail.com>
2021-11-12 08:57:17 +0100

    canvis

--- tareas-hacer/app.js
+++ tareas-hacer/app.js
@@ -1,5 +1,7 @@
+const { inquirerMenu } = require("./helpers/inquirer");
 require("colors");
-const { mostrarMenu, pausa } = require("./helpers/mensajes");
+
+//const { mostrarMenu, pausa } = require("./helpers/mensajes");
 
 console.clear();
 
@@ -7,7 +9,7 @@ const main = async () => {
   let opt = "";
 
   do {
-    opt = await mostrarMenu();
+    opt = await inquirerMenu();
     console.log({ opt });
     if (opt !== '0') await pausa();
   } while (opt !== "0");
